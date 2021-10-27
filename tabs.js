3a938e2bfc18364cd7a16006892df54da63c11e4
Antoine Joulia <antoine.joulia@onera.fr>
2021-10-27 08:17:46 +0200

    Destination tabs fully functional

--- tabs.js
+++ tabs.js
@@ -42,6 +42,12 @@ function changeTabPanel(e) {
   const tabContainer = targetTab.parentNode;
   const mainContainer = tabContainer.parentNode;
 
+  tabContainer
+    .querySelector('[aria-selected="true"]')
+    .setAttribute("aria-selected", false);
+
+  targetTab.setAttribute("aria-selected", true);
+
   mainContainer
     .querySelectorAll('[role="tabpanel"]')
     .forEach((panel) => panel.setAttribute("hidden", true));
